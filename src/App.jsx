7b71f723d6a399ca3eca078928fd1f504ecb0544
Zainuri Mahmud <zainuri.mahmud@bni.co.id>
2024-07-22 09:22:11 +0700

    create custom component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,24 @@
 import "./App.css";
 
+function WebTitle() {
+  return <h1>Netflix Clone</h1>;
+}
+
+function Articles() {
+  return (
+    <article>
+      <h3>Artikel Pertama</h3>
+      <p>Ini adalah artikel pertama</p>
+    </article>
+  );
+}
+
 function App() {
   return (
-    <>
-      <p>Netflix Clone</p>
-    </>
+    <main>
+      <WebTitle />
+      <Articles />
+    </main>
   );
 }
 
