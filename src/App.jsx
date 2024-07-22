fb74448f6e1af7a60f143e28b980918ebf0ca8ac
Zainuri Mahmud <zainuri.mahmud@bni.co.id>
2024-07-22 16:57:49 +0700

    add useEffect lifecycle

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import "./App.css";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 function WebTitle({ titleProp, description }) {
   const [title, setTitle] = useState(titleProp);
@@ -18,7 +18,6 @@ function WebTitle({ titleProp, description }) {
 }
 
 function Articles({ title, description }) {
-  console.log(title, description);
   return (
     <article>
       <h3>{title}</h3>
@@ -28,13 +27,30 @@ function Articles({ title, description }) {
 }
 
 function App() {
+  const [article, setArticle] = useState("");
+  const [counter, setCounter] = useState(1);
+
+  useEffect(() => {
+    fetch("https://dummyjson.com/posts/" + counter)
+      .then((res) => res.json())
+      .then((data) => setArticle(data));
+  }, [counter]);
+
+  function nextArticle() {
+    setCounter(counter + 1);
+  }
+
+  function prevArticle() {
+    setCounter(counter - 1);
+  }
+
   return (
     <main>
       <WebTitle titleProp="Netflix" description="Ini adalah website netflix" />
-      <Articles
-        title="Artikel Pertama"
-        description="Ini adalah artikel pertama"
-      />
+      <Articles title={article.title} description={article.body} />
+      <button onClick={prevArticle}>Next Article</button>
+      {counter}
+      <button onClick={nextArticle}>Next Article</button>
     </main>
   );
 }
