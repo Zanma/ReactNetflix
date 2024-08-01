c41e94dd06dbb296b37526e937ede7ad449c10b5
Zainuri Mahmud <zainurimahmud32@gmail.com>
2024-08-01 23:22:36 +0700

    add lib daisyy & framer

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -4,12 +4,14 @@ import { useAtom } from "jotai";
 import { languageAtom } from "../jotai/atoms";
 
 function Home() {
-  const [language,] = useAtom(languageAtom)
-  console.log(language)
+  const [language] = useAtom(languageAtom);
+  console.log(language);
 
   return (
     <Layout>
-      <h3>{language === "en" ? "Watch Netflix Free!" : "Tonton Netflix Gratis"}</h3>
+      <h3>
+        {language === "en" ? "Watch Netflix Free!" : "Tonton Netflix Gratis"}
+      </h3>
     </Layout>
   );
 }
--- src/Pages/Navbar.jsx
+++ src/Pages/Navbar.jsx
@@ -1,19 +1,26 @@
-import React from 'react'
-import { useAtom } from 'jotai'
-import { languageAtom } from '../jotai/atoms'
+import React from "react";
+import { useAtom } from "jotai";
+import { languageAtom } from "../jotai/atoms";
 
 const Navbar = () => {
-    const [, setLanguage] = useAtom(languageAtom)
+  const [, setLanguage] = useAtom(languageAtom);
 
-    return (
-        <div className='Navbar'>
-            <h1>Netflix Clone</h1>
-            <div>
-                <button onClick={() => setLanguage("id")}>Indonesia</button>
-                <button onClick={() => setLanguage("en")}>English</button>
-            </div>
-        </div>
-    )
-}
+  return (
+    <div className="Navbar">
+      <h1>Netflix Clone</h1>
+      <div>
+        <button
+          className="btn btn-outline mr-2"
+          onClick={() => setLanguage("id")}
+        >
+          Indonesia
+        </button>
+        <button className="btn btn-outline" onClick={() => setLanguage("en")}>
+          English
+        </button>
+      </div>
+    </div>
+  );
+};
 
-export default Navbar
+export default Navbar;
