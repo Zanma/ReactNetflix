751fae27eecfa4742570d37754ce7d5a7c9f0b4d
Zainuri Mahmud <zainurimahmud32@gmail.com>
2024-08-05 06:54:40 +0700

    create data JSON

--- src/main.jsx
+++ src/main.jsx
@@ -1,24 +1,24 @@
-import React from 'react'
-import { RouterProvider, createBrowserRouter } from 'react-router-dom'
-import ReactDOM from 'react-dom/client'
-import './index.css'
+import React from "react";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import ReactDOM from "react-dom/client";
+import "./index.css";
 
-import Home from './Pages/Home.jsx'
-import Browse from './Pages/Browse.jsx'
+import Home from "./Pages/Landing";
+import Browse from "./Pages/Browse";
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <Home />
-  }
-  , {
+    element: <Home />,
+  },
+  {
     path: "/Browse",
-    element: <Browse />
-  }
-])
+    element: <Browse />,
+  },
+]);
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <RouterProvider router={router} />
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
